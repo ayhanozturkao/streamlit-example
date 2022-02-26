3b8a1dc18ebd23ef094e903410cb9581fbf2195d
ayhanozturkao <51856610+ayhanozturkao@users.noreply.github.com>
2022-02-26 14:24:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    total_points = st.slider("Number of points in spiral", 1, 500, 200)
+    num_turns = st.slider("Number of turns in spiral", 1, 10, 9)
 
     Point = namedtuple('Point', 'x y')
     data = []
@@ -33,6 +33,6 @@ with st.echo(code_location='below'):
         y = radius * math.sin(angle)
         data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=600, width=700)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
